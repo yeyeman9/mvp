a6b9501178e2d6e13ff561b09c76887e81e91a5f
Jose Ramos <yeyeman9@gmail.com>
2017-02-03 03:49:34 +0000

    added audio url to the audio json file

--- app/controllers/api/audios_controller.rb
+++ app/controllers/api/audios_controller.rb
@@ -1,12 +1,12 @@
 class Api::AudiosController < ApplicationController
   
   def index
-    render json: Audio.all
+    render :json => Audio.all.as_json(methods: :audio)
   end
   
   def show
     @audio = Audio.find(params[:id])
-    render json: @audio.audio.url
+    render :json => @audio.as_json(methods: :audio)
   end
   
 end
