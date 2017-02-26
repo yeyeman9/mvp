7882387ee66b310526fe8208ca783db71b538bd8
Jose Ramos <yeyeman9@gmail.com>
2017-02-26 03:29:52 +0000

    added user object in log in call

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -2,10 +2,13 @@ class Api::AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
   
   def authenticate
+     email = params[:email]
      command = AuthenticateUser.call(params[:email], params[:password])
      
      if command.success?
-       render json: { auth_token: command.result }
+       render json: { 
+         user:  User.find_by_email(email),
+         auth_token: command.result }
      else
        render json: { error: command.errors }, status: :unauthorized
      end
